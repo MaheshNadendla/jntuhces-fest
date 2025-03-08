8d38e97a196bdd3f9b592d501d06cd9122321ff0
MaheshNadendla <maheshnadendla147@gmail.com>
2025-03-09 04:00:27 +0530

    photos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import navImage from './assets/nav.svg'
+import BG from './assets/AI.jpg'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Container, AppBar, Toolbar, Typography, Button } from "@mui/material";
+import './App.css'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppBar position="static" sx={{ height : '100vh', backgroundImage: `url(${BG})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' , }} >
+      <div style={{height:'100px', display :'flex',justifyContent : 'space-around' , alignItems: 'center', backgroundImage: `url(${navImage})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat',backgroundPosition: 'bottom'}}>
+        <Typography variant="h6" sx={{ flexGrow: 1,  }}>
+            Techfest Clone
+          </Typography>
+        <Button color="inherit">Home</Button>
+        <Button color="inherit">Events</Button>
+        <Button color="inherit">About</Button>
+      </div>
+
+      <Container   >
+      <Typography variant="h3" gutterBottom>
+        Welcome to Techfest Clone
+      </Typography>
+      <Typography variant="body1">
+        This is a static React-based clone using Material UI.
+      </Typography>
+    </Container>
+
+    </AppBar>
   );
 }
 
